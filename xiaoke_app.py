4e96c71a699c6801af229898b7e1b54010b46617
luwenrui3055-source <luwenrui3055@gmail.com>
2026-08-10 16:11:14 +0800

    Update xiaoke_app.py

--- xiaoke_app.py
+++ xiaoke_app.py
@@ -6,4 +6,4 @@ import os
 from xiaoke_gateway_api import create_app
 app = create_app(os.environ.get('XIAOKE_DB_PATH', 'data/xiaoke.sqlite'))
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=int(os.environ.get('PORT', '3010')), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', '8080')), debug=False)

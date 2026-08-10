d9bac211f7edce8e613359ba35c9fe184b2db6ae
luwenrui3055-source <luwenrui3055@gmail.com>
2026-08-10 15:53:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,3 @@ from xiaoke_gateway_api import create_app
 
 app = create_app(os.environ.get('XIAOKE_DB_PATH', 'data/xiaoke.sqlite'))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', '8080')), debug=False)
-
